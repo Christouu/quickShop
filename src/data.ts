ec2c98fe8e036ba716f151d1f3037c9d2a9622a2
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-06-28 11:17:12 -0700

    added Slider COmponnet

---
+++ src/data.ts
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "assets/imgSlider1.jpg",
+    title: "Над +10 000 продукта",
+    desc: "Всеки един от наще магазини има разнообразие от продукти, от които да избирате",
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "assets/registerImg.jpg",
+    title: "Ниски цени!",
+    desc: "Цените ни са най - добрите на пазара",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "assets/imgSlider2.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
