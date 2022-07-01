5947e12a5c4a4f69f856fb3952c14978820ccd59
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-07-01 13:39:30 -0700

    added Register page

---
+++ src/pages/Register/Register.styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("assets/registerImg.jpg") center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+`;
+
+export const H1 = styled.h1`
+  font-size: 28px;
+  font-weight: 300;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+export const Span = styled.span`
+  font-size: 15px;
+  margin: 20px 0px;
+`;
+
+export const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
