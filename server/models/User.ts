6f5d6b42697898c9ffa77589da16b7e73833ff0a
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-07-04 11:52:15 -0700

    added Register and Login route

---
+++ server/models/User.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+export interface UserDocument extends mongoose.Document {
+  username: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
