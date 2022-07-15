53889cab6b129d15369aa3ac5a9ee0dc7938d420
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-07-15 11:54:12 -0700

    added featured component and a chart component

--- client/src/components/adminFeatured/AdminFeatured.styles.ts
+++ client/src/components/adminFeatured/AdminFeatured.styles.ts
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
+interface Props {
+  arrow: string;
+}
+
 export const Container = styled.div`
-  flex: 2;
+  flex: 1;
   -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
   box-shadow: 2px 4px 10px 1px rgba(201, 201, 201, 0.47);
-  padding: 10px;
+  padding: 10px 30px;
 `;
 
 export const Top = styled.div`
@@ -19,4 +23,55 @@ export const H1 = styled.h1`
   font-weight: 500;
 `;
 
-export const Bottom = styled.div``;
+export const Bottom = styled.div`
+  display: flex;
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FeaturedChart = styled.div`
+  width: 150px;
+  height: 150px;
+`;
+
+export const Title = styled.p`
+  font-weight: 500;
+  color: gray;
+`;
+
+export const Amount = styled.p`
+  font-size: 30px;
+`;
+
+export const Description = styled.p`
+  font-weight: 300;
+  font-size: 18px;
+  text-align: center;
+`;
+
+export const Summary = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Item = styled.div`
+  text-align: center;
+`;
+
+export const ItemTitle = styled.div`
+  font-size: 20px;
+  color: gray;
+`;
+
+export const ItemResult = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  color: ${(props: Props) => (props.arrow === "positive" ? "green" : "red")};
+`;
+
+export const ResultAmount = styled.div``;
--- client/src/components/adminFeatured/AdminFeatured.tsx
+++ client/src/components/adminFeatured/AdminFeatured.tsx
@@ -1,6 +1,24 @@
-import { Bottom, Container, H1, Top } from "./AdminFeatured.styles";
+import {
+  Amount,
+  Bottom,
+  Container,
+  Description,
+  FeaturedChart,
+  H1,
+  Item,
+  ItemResult,
+  ItemTitle,
+  ResultAmount,
+  Summary,
+  Title,
+  Top,
+} from "./AdminFeatured.styles";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const AdminFeatured = () => {
   return (
@@ -9,7 +27,41 @@ const AdminFeatured = () => {
         <H1>Total Revenue</H1>
         <MoreVertIcon fontSize="medium" />
       </Top>
-      <Bottom></Bottom>
+      <Bottom>
+        <FeaturedChart>
+          <CircularProgressbar value={70} text="70%" strokeWidth={5} />
+        </FeaturedChart>
+        <Title>Total sales made today.</Title>
+        <Amount>$420</Amount>
+        <Description>
+          adsasdasdasdsadasdsadasdsadasdsadsadasfwqfwqqqwrfqhasudgasou
+          gaudpisfgwaepiuytgfuiewgfuadsa
+        </Description>
+
+        <Summary>
+          <Item>
+            <ItemTitle>Target</ItemTitle>
+            <ItemResult arrow="negative">
+              <KeyboardArrowDownIcon />
+              <ResultAmount>$12k</ResultAmount>
+            </ItemResult>
+          </Item>
+          <Item>
+            <ItemTitle>Last Week</ItemTitle>
+            <ItemResult arrow="negative">
+              <KeyboardArrowDownIcon />
+              <ResultAmount>$8k</ResultAmount>
+            </ItemResult>
+          </Item>
+          <Item>
+            <ItemTitle>Last Month</ItemTitle>
+            <ItemResult arrow="positive">
+              <KeyboardArrowUpIcon />
+              <ResultAmount>$18k</ResultAmount>
+            </ItemResult>
+          </Item>
+        </Summary>
+      </Bottom>
     </Container>
   );
 };
