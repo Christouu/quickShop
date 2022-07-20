d641a92fe464196383543b8a9fe824bbedad3917
Kristou <kristou.svetoslavov.petkov@gmail.com>
2022-07-19 17:24:25 -0700

    starting cart page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
+import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
-import "./App.css";
-import NotFound from "./components/notFound/NotFound";
-import About from "./pages/About/About";
+
+import Work from "./pages/Work/Work";
+import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
-import ProductInfo from "./pages/ProductInfo/ProductInfo";
+import Admin from "./pages/Admin/Admin";
 import Products from "./pages/Products/Products";
 import Register from "./pages/Register/Register";
-import Work from "./pages/Work/Work";
-import ProductsOnSale from "./pages/ProductsOnSale/ProductsOnSale";
-import Admin from "./pages/Admin/Admin";
+import NotFound from "./components/notFound/NotFound";
 import AdminUsers from "./pages/AdminUsers/AdminUsers";
+import ProductInfo from "./pages/ProductInfo/ProductInfo";
 import AdminProducts from "./pages/AdminProducts/AdminProducts";
+import ProductsOnSale from "./pages/ProductsOnSale/ProductsOnSale";
 
 function App() {
   const user = useSelector((state: RootState) => state.user.currentUser);
@@ -26,6 +28,7 @@ function App() {
       <Route path="/products/:id" element={<ProductInfo />} />
       <Route path="/work" element={<Work />} />
       <Route path="/about" element={<About />} />
+      <Route path="/cart" element={<Cart />} />
       <Route path="/admin">
         <Route index element={<Admin />} />
         <Route path="users" element={<AdminUsers />} />
---
+++ client/src/pages/Cart/Cart.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/footer/Footer";
+import Navigation from "../../components/navigation/Navigation";
+import { Container } from "./Cart.styles";
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navigation />
+      asdsadsa
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
