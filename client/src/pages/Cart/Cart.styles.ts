3efcc9fd332abf3fb10a0da05ae3df455921e6db
kristou799@gmail.com <kristiyan.svetoslavov.petkov@gmail.com>
2022-07-20 12:03:26 +0300

    added Cart page

--- client/src/pages/Cart/Cart.styles.ts
+++ client/src/pages/Cart/Cart.styles.ts
@@ -1,5 +1,163 @@
 import styled from "styled-components";
 
+interface Props {
+  option?: string;
+}
+
 export const Container = styled.div`
   background-color: #fcf1ed;
+`;
+
+export const Wrapper = styled.div`
+  padding: 20px 50px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 44px;
+`;
+
+export const WrapperWhite = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  margin-bottom: 20px;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 50px;
+  margin-bottom: 40px;
+`;
+
+export const TopButton = styled.button<Props>`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props: Props) => props.option === "filled" && "none"};
+  background-color: ${(props: Props) =>
+    props.option === "filled" ? "black" : "white"};
+
+  color: ${(props: Props) => (props.option === "filled" ? "white" : "black")};
+  transition: all 0.5s ease;
+
+  &:hover {
+    transform: scale(1.3);
+  }
+`;
+
+export const TopText = styled.span`
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 26px;
+  text-align: center;
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 0px;
+`;
+
+export const Info = styled.div`
+  flex: 3;
+`;
+
+export const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0px;
+`;
+
+export const ProductDetails = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+export const ProductImage = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: contain;
+`;
+
+export const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const ProductName = styled.span``;
+
+export const ProductQuantity = styled.span``;
+
+export const ProductPrice = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProductPriceAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const ProductPriceAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+export const Price = styled.div`
+  font-size: 30px;
+  font-weight: 400;
+  margin: 5px;
+`;
+
+export const HR = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+export const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+export const SummaryTitle = styled.h1`
+  font-weight: 500;
+`;
+
+export const SummaryItem = styled.div<Props>`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.option === "total" && "700"};
+  font-size: ${(props) => props.option === "total" && "26px"};
+`;
+
+export const SummaryItemText = styled.span``;
+
+export const SummaryItemPrice = styled.span``;
+
+export const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  transition: all 0.5s ease;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -1,12 +1,165 @@
+import {
+  Bottom,
+  Button,
+  Container,
+  Details,
+  HR,
+  Info,
+  Price,
+  Product,
+  ProductDetails,
+  ProductImage,
+  ProductName,
+  ProductPrice,
+  ProductPriceAmount,
+  ProductPriceAmountContainer,
+  ProductQuantity,
+  Summary,
+  SummaryItem,
+  SummaryItemPrice,
+  SummaryItemText,
+  SummaryTitle,
+  Title,
+  Top,
+  TopButton,
+  TopText,
+  Wrapper,
+  WrapperWhite,
+} from "./Cart.styles";
 import Footer from "../../components/footer/Footer";
 import Navigation from "../../components/navigation/Navigation";
-import { Container } from "./Cart.styles";
+import Line from "../../components/line/Line";
+
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const Cart = () => {
   return (
     <Container>
       <Navigation />
-      asdsadsa
+      <Wrapper>
+        <Title>Your Bag</Title>
+        <Line></Line>
+        <Top>
+          <TopButton option="normal">Continure Shopping</TopButton>
+          <TopText>Shopping Bag(2)</TopText>
+          <TopButton option="filled">Checkout Now </TopButton>
+        </Top>
+        <WrapperWhite>
+          <Bottom>
+            <Info>
+              <Product>
+                <ProductDetails>
+                  <ProductImage src="https://images.prom.ua/2081880662_voda-vodichka-05l.jpg" />
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> Vodichka
+                    </ProductName>
+                    <ProductQuantity>
+                      <b>Quantity:</b> 0.5 L
+                    </ProductQuantity>
+                  </Details>
+                </ProductDetails>
+                <ProductPrice>
+                  <ProductPriceAmountContainer>
+                    <AddIcon />
+                    <ProductPriceAmount>2</ProductPriceAmount>
+                    <RemoveIcon />
+                  </ProductPriceAmountContainer>
+                  <Price>$ 30</Price>
+                </ProductPrice>
+              </Product>
+              <HR />
+              <Product>
+                <ProductDetails>
+                  <ProductImage src="https://images.prom.ua/2081880662_voda-vodichka-05l.jpg" />
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> Vodichka
+                    </ProductName>
+                    <ProductQuantity>
+                      <b>Quantity:</b> 0.5 L
+                    </ProductQuantity>
+                  </Details>
+                </ProductDetails>
+                <ProductPrice>
+                  <ProductPriceAmountContainer>
+                    <AddIcon />
+                    <ProductPriceAmount>2</ProductPriceAmount>
+                    <RemoveIcon />
+                  </ProductPriceAmountContainer>
+                  <Price>$ 30</Price>
+                </ProductPrice>
+              </Product>
+              <HR />
+              <Product>
+                <ProductDetails>
+                  <ProductImage src="https://images.prom.ua/2081880662_voda-vodichka-05l.jpg" />
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> Vodichka
+                    </ProductName>
+                    <ProductQuantity>
+                      <b>Quantity:</b> 0.5 L
+                    </ProductQuantity>
+                  </Details>
+                </ProductDetails>
+                <ProductPrice>
+                  <ProductPriceAmountContainer>
+                    <AddIcon />
+                    <ProductPriceAmount>2</ProductPriceAmount>
+                    <RemoveIcon />
+                  </ProductPriceAmountContainer>
+                  <Price>$ 30</Price>
+                </ProductPrice>
+              </Product>
+              <HR />
+              <Product>
+                <ProductDetails>
+                  <ProductImage src="https://images.prom.ua/2081880662_voda-vodichka-05l.jpg" />
+                  <Details>
+                    <ProductName>
+                      <b>Product:</b> Vodichka
+                    </ProductName>
+                    <ProductQuantity>
+                      <b>Quantity:</b> 0.5 L
+                    </ProductQuantity>
+                  </Details>
+                </ProductDetails>
+                <ProductPrice>
+                  <ProductPriceAmountContainer>
+                    <AddIcon />
+                    <ProductPriceAmount>2</ProductPriceAmount>
+                    <RemoveIcon />
+                  </ProductPriceAmountContainer>
+                  <Price>$ 30</Price>
+                </ProductPrice>
+              </Product>
+              <HR />
+            </Info>
+            <Summary>
+              <SummaryTitle>Order Summary</SummaryTitle>
+              <SummaryItem>
+                <SummaryItemText>Subtotal</SummaryItemText>
+                <SummaryItemPrice>$ 80</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Estimated Shipping</SummaryItemText>
+                <SummaryItemPrice>$ 10</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem>
+                <SummaryItemText>Discount</SummaryItemText>
+                <SummaryItemPrice>$ -2.99</SummaryItemPrice>
+              </SummaryItem>
+              <SummaryItem option="total">
+                <SummaryItemText>Total</SummaryItemText>
+                <SummaryItemPrice>$ 90</SummaryItemPrice>
+              </SummaryItem>
+              <Button>Checkout Now</Button>
+            </Summary>
+          </Bottom>
+        </WrapperWhite>
+      </Wrapper>
       <Footer />
     </Container>
   );
